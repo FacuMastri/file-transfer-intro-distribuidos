17e0320265d20e0a22fce798140c4da78572183d
Santiago Tissoni <santi.tissoni@gmail.com>
2022-09-18 23:29:13 -0300

    Formatting

--- client/upload.py
+++ client/upload.py
@@ -2,24 +2,28 @@ import argparse
 import logging
 from socket import AF_INET, SOCK_DGRAM, socket
 
-DEFAULT_SERVER = 'localhost'
+DEFAULT_SERVER = "localhost"
 DEFAULT_SERVER_PORT = 12000
 DEFAULT_DEBUG_LEVEL = logging.INFO
 BUFFER = 1024
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('-v', '--verbose', action='store_true', help="increase output verbosity")
-parser.add_argument('-q', '--quiet', action='store_true', help="decrease output verbosity")
-parser.add_argument('-H', '--host', metavar='host', required=False, help="service ip address")
-parser.add_argument('-p', '--port', metavar='port', required=False, help="service port")
-parser.add_argument('-s', '--src', metavar='path', required=True, help="source file path")
-parser.add_argument('-n', '--name', metavar='path', required=True, help="file name")
+parser.add_argument(
+    "-v", "--verbose", action="store_true", help="increase output verbosity"
+)
+parser.add_argument(
+    "-q", "--quiet", action="store_true", help="decrease output verbosity"
+)
+parser.add_argument("-H", "--host", metavar="host", help="service ip address")
+parser.add_argument("-p", "--port", metavar="port", help="service port")
+parser.add_argument("-s", "--src", metavar="path", help="source file path")
+parser.add_argument("-n", "--name", metavar="path", required=True, help="file name")
 
 args = parser.parse_args()
 
 if args.verbose and args.quiet:
-    raise ValueError('-v and -q options not compatible')
+    raise ValueError("-v and -q options not compatible")
 
 if args.quiet:
     DEBUG_LEVEL = logging.ERROR
@@ -31,14 +35,14 @@ else:
 SERVER = DEFAULT_SERVER if args.host is None else args.host
 SERVER_PORT = DEFAULT_SERVER_PORT if args.port is None else args.port
 
-logging.basicConfig(format='[%(levelname)s] %(message)s', level=DEBUG_LEVEL)
-logging.debug(f'Setting {logging.getLevelName(DEBUG_LEVEL)} log level')
+logging.basicConfig(format="[%(levelname)s] %(message)s", level=DEBUG_LEVEL)
+logging.debug(f"Setting {logging.getLevelName(DEBUG_LEVEL)} log level")
 
 client_socket = socket(AF_INET, SOCK_DGRAM)
 svr_addr = (SERVER, SERVER_PORT)
 
-logging.info('FTP client up')
-logging.info(f'FTP server address {SERVER}:{SERVER_PORT}')
+logging.info("FTP client up")
+logging.info(f"FTP server address {SERVER}:{SERVER_PORT}")
 
 file_bytes = 0
 total_bytes = 0
@@ -46,29 +50,30 @@ packets_sended = 0
 
 HEADER_LENGHT = len(args.name)
 
+
 def add_headers(payload):
     name = args.name
-    packet_data = bytes(name, 'utf-8') + payload
+    packet_data = bytes(name, "utf-8") + payload
     return packet_data
+
 
 logging.info(f"Uploading {args.src} to FTP server")
 
 with open(args.src, "rb") as f:
     data = f.read(BUFFER - HEADER_LENGHT)
-    while(data):
+    while data:
         file_bytes += len(data)
-        total_bytes += (len(data) + HEADER_LENGHT)
+        total_bytes += len(data) + HEADER_LENGHT
         packets_sended += 1
-        
+
         packet_data = add_headers(data)
         logging.debug(f"Sending {len(packet_data)} bytes to {SERVER}:{SERVER_PORT}")
         logging.debug(f"First 20 bytes sended: {list(packet_data[0:20])}")
         client_socket.sendto(packet_data, svr_addr)
-        
+
         data = f.read(BUFFER - HEADER_LENGHT)
-    
+
     logging.info("Upload complete!")
     logging.info(f"Total bytes sended {total_bytes}")
     logging.info(f"Total file bytes sended {file_bytes}")
     logging.info(f"Total packets sended {packets_sended}")
-
--- server/server.py
+++ server/server.py
@@ -9,16 +9,20 @@ BUFFER = 1024
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('-v', '--verbose', action='store_true', help="increase output verbosity")
-parser.add_argument('-q', '--quiet', action='store_true', help="decrease output verbosity")
-parser.add_argument('-H', '--host', metavar='host', required=False, help="service ip address")
-parser.add_argument('-p', '--port', metavar='port', required=False, help="service port")
-parser.add_argument('-s', '--storage', metavar='path', required=False, help="storage dir path")
+parser.add_argument(
+    "-v", "--verbose", action="store_true", help="increase output verbosity"
+)
+parser.add_argument(
+    "-q", "--quiet", action="store_true", help="decrease output verbosity"
+)
+parser.add_argument("-H", "--host", metavar="host", help="service ip address")
+parser.add_argument("-p", "--port", metavar="port", help="service port")
+parser.add_argument("-s", "--storage", metavar="path", help="storage dir path")
 
 args = parser.parse_args()
 
 if args.verbose and args.quiet:
-    raise Exception('Verbose and quiet options are not compatible')
+    raise Exception("Verbose and quiet options are not compatible")
 
 if args.quiet:
     DEBUG_LEVEL = logging.ERROR
@@ -29,52 +33,58 @@ else:
 
 SERVER_PORT = DEFAULT_SERVER_PORT if args.port is None else int(args.port)
 
-logging.basicConfig(format='[%(levelname)s] %(message)s', level=DEBUG_LEVEL)
+logging.basicConfig(format="[%(levelname)s] %(message)s", level=DEBUG_LEVEL)
 logging.debug(f"Setting {logging.getLevelName(DEBUG_LEVEL)} log level")
 
 server_socket = socket(AF_INET, SOCK_DGRAM)
 
 try:
-    server_socket.bind(('', SERVER_PORT))
+    server_socket.bind(("", SERVER_PORT))
 except Exception as e:
-    logging.error('Port already in use')
+    logging.error("Port already in use")
     raise e
 
-logging.info(f'FTP server up in port {SERVER_PORT}')
+logging.info(f"FTP server up in port {SERVER_PORT}")
 
 data, client_address = server_socket.recvfrom(BUFFER)
 name = data[0:11]
 logging.debug(f"Filename received: {name.decode()}")
 
-file = open(f"server/{name.decode()}",'wb')
+file = open(f"server/{name.decode()}", "wb")
 
 total_bytes_written = 0
 total_bytes_received = 0
 total_packets = 0
+
 
 def process_packet(data, header_bytes):
     header = data[0:header_bytes]
     data = data[header_bytes:]
     return header, data
 
+
 try:
-    while(data):
+    while data:
         total_bytes_received += len(data)
         total_packets += 1
-        logging.debug(f"Data received from client {client_address[0]}:{client_address[1]}: {len(data)} bytes")
+        logging.debug(
+            f"Data received from client {client_address[0]}:{client_address[1]}: {len(data)} bytes"
+        )
         logging.debug(f"First 20 bytes received: {list(data[0:20])}")
-        
+
         # Aqui puedo extraer los datos del archivo del paquete recibido
-        header, data = process_packet(data, 11) # Aca extraigo headers y cosas asi que vayamos definiendo
+        header, data = process_packet(
+            data, 11
+        )  # Aca extraigo headers y cosas asi que vayamos definiendo
         file.write(data)
         total_bytes_written += len(data)
 
         logging.debug(f"Writting data to {file.name}: {len(data)} bytes")
         logging.debug(f"First 20 bytes written: {list(data[0:20])}")
-        
+
         server_socket.settimeout(2)
         data, client_address = server_socket.recvfrom(BUFFER)
-        
+
 except:
     file.close()
     server_socket.close()
