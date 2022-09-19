1444f11e412c2c51c95bdb6b99cf196b596804f2
Santiago Tissoni <santi.tissoni@gmail.com>
2022-09-19 20:31:35 -0300

    Updated tests

--- tests/test_tests.py
+++ tests/test_tests.py
@@ -4,7 +4,7 @@ from src.server.utils import decode_packet
 
 def test_construct_packet():
     payload = bytes("esto es una prueba", "utf-8")
-    packet = construct_packet(12, "prueba.txt", 50, payload)
+    packet = construct_packet(257, "prueba.txt", 50, payload)
     # Se puede imprimir
     print(packet)
     # Vemos el tipo
@@ -14,13 +14,18 @@ def test_construct_packet():
 
     assert packet[0] == 0
     assert packet[1] == 0
-    assert packet[2] == 0
-    assert packet[3] == 12
+    assert packet[2] == 1
+    assert packet[3] == 1
 
     assert packet[4] == 0
     assert packet[5] == 0
     assert packet[6] == 0
     assert packet[7] == 50
+
+    assert packet[8] == 0
+    assert packet[9] == 0
+    assert packet[10] == 0
+    assert packet[11] == 10
 
 
 def test_decode_packet():
