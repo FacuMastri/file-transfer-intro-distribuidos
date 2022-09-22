d9d19d946d97e33a770941b8ed2d35487b5740b1
Facundo Mastricchio <facundomastricchio@gmail.com>
2022-09-21 21:58:59 -0300

    Rename upload arguments

--- src/upload.py
+++ src/upload.py
@@ -8,7 +8,7 @@ DEFAULT_SERVER_PORT = 12000
 DEFAULT_DEBUG_LEVEL = logging.INFO
 BUFFER = 1024
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(description="FTP server - flags for upload command")
 
 parser.add_argument(
     "-v", "--verbose", action="store_true", help="increase output verbosity"
@@ -16,10 +16,10 @@ parser.add_argument(
 parser.add_argument(
     "-q", "--quiet", action="store_true", help="decrease output verbosity"
 )
-parser.add_argument("-H", "--host", metavar="host", help="service ip address")
-parser.add_argument("-p", "--port", metavar="port", help="service port")
-parser.add_argument("-s", "--src", metavar="path", help="source file path")
-parser.add_argument("-n", "--name", metavar="path", required=True, help="file name")
+parser.add_argument("-H", "--host", metavar="addr", help="server ip address")
+parser.add_argument("-p", "--port", metavar="port", help="server port")
+parser.add_argument("-s", "--src", metavar="filepath", help="source file path")
+parser.add_argument("-n", "--name", metavar="filename", required=True, help="file name")
 
 args = parser.parse_args()
 
